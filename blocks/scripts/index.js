de2c7b1c1a8c66ce51dae8cf401ca30d8fea7151
Daniil Belogur <katokinawa@icloud.com>
2022-08-11 21:50:11 +0300

    feat: js code for popup

--- blocks/scripts/index.js
+++ blocks/scripts/index.js
@@ -1,3 +1,49 @@
-let editButton = profile.querySelector('.profile__edit-button');
-let addButton = profile.querySelector('.profile__add-button');
-let
+/* button */
+let editButton = document.querySelector('.profile__edit-button');
+
+let closeButton = document.querySelector('.popup__close-button')
+let addButton = document.querySelector('.profile__add-button');
+let saveButton = document.querySelector('.popup__save-button');
+let likeButton = document.querySelector('.photo-flex__like-button')
+
+/* form */
+let formElement = document.querySelector('.popup__form-container');
+let nameInput = document.querySelector('#nameInput');
+let jobInput = document.querySelector('#jobInput');
+
+/* other */
+let popupShow = document.querySelector('#popup')
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+
+/* main js code */
+
+function popupFormValue() {
+  nameInput.setAttribute('value', profileName.textContent);
+  jobInput.setAttribute('value', profileJob.textContent);
+  console.log('1');
+}
+
+function openPopup() {
+  popupFormValue();
+  popupShow.classList.add('popup_opened');
+  console.log('2');
+}
+
+function closePopup() {
+  popupShow.classList.remove('popup_opened');
+  console.log('3');
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+saveButton.addEventListener('click', closePopup)
+
+function formSumbitHandler(evt) {
+  evt.preventDefault();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  console.log('4');
+}
+
+formElement.addEventListener('sumbit', formSumbitHandler);
