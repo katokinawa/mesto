fee97d57ac0626693bbf8b0f1307396b87efd238
Daniil Belogur <katokinawa@icloud.com>
2022-08-12 16:52:03 +0300

    fix: popup pos and title overflow

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,6 @@
 /* button */
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button')
-let addButton = document.querySelector('.profile__add-button');
-let saveButton = document.querySelector('.popup__save-button');
-let likeButton = document.querySelector('.photo-flex__like-button')
 
 /* form */
 let formElement = document.querySelector('#form');
@@ -14,16 +11,8 @@ let jobInput = document.querySelector('#jobInput');
 let popupShow = document.querySelector('#popup')
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
-let PageOpacity = document.querySelector('.page');
 /* main js code */
 
-function backgroundOpacity() {
-  PageOpacity.style = 'opacity: 0.5';
-}
-
-function backgroundOpacityNormal() {
-  PageOpacity.style = 'opacity: 1';
-}
 
 function popupFormValue() {
   nameInput.setAttribute('value', profileName.textContent);
@@ -32,18 +21,15 @@ function popupFormValue() {
 
 function openPopup() {
   popupFormValue();
-  backgroundOpacity();
   popupShow.classList.add('popup_opened');
 }
 
 function closePopup() {
-  backgroundOpacityNormal()
   popupShow.classList.remove('popup_opened');
 }
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-saveButton.addEventListener('click', closePopup)
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
