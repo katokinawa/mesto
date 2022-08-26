186d2e1fbef74e1ea9d7c55b2d9663b61da717d6
Daniil Belogur <katokinawa@icloud.com>
2022-08-26 13:08:14 +0300

    feat: add popup add-button

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,17 @@
 /* button */
 let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button')
+let closeButton = document.querySelectorAll('.popup__close-button')
+let addButton = document.querySelector('.profile__add-button');
 
 /* form */
 let formElement = document.querySelector('#form');
 let nameInput = document.querySelector('#nameInput');
 let jobInput = document.querySelector('#jobInput');
+let itemNameInput = document.querySelector('#itemNameInput');
+let itemUrlInput = document.querySelector('#itemUrlInput');
 
 /* other */
-let popupShow = document.querySelector('#popup')
+let popupShow = document.querySelectorAll('#popup')
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 /* main js code */
@@ -17,24 +20,59 @@ let profileJob = document.querySelector('.profile__subtitle');
 function popupFormValue() {
   nameInput.setAttribute('value', profileName.textContent);
   jobInput.setAttribute('value', profileJob.textContent);
-}
+};
 
 function openPopup() {
   popupFormValue();
-  popupShow.classList.add('popup_opened');
-}
+  popupShow[0].classList.add('popup_opened');
+};
+
+function openPopupAdd() {
+  popupShow[1].classList.add('popup_opened');
+};
 
 function closePopup() {
-  popupShow.classList.remove('popup_opened');
-}
+  popupShow[0].classList.remove('popup_opened');
+  popupShow[1].classList.remove('popup_opened');
+};
+
 
 editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+closeButton[0].addEventListener('click', closePopup);
+closeButton[1].addEventListener('click', closePopup);
+addButton.addEventListener('click', openPopupAdd);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closePopup();
-}
+};
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
