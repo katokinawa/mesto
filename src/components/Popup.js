977cef9c72c552e0d6e51d49a7cc704642c1a71a
Daniil Belogur <katokinawa@icloud.com>
2022-10-27 13:57:44 +0300

    fix: PopupWithImage

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,6 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
-
   };
   // Открыть попап
   open() {
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,8 +3,8 @@ import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._imageModal = document.querySelector('.popup__image');
-    this._imageModalTitle = document.querySelector('.popup__title-image');
+    this._imageModal = this._popup.querySelector('.popup__image');
+    this._imageModalTitle = this._popup.querySelector('.popup__title-image');
   }
   // вставляем в попап с фуллскрин картинкой его тайтл, алт, и саму картинку
   // Также перезаписываем родительский метод
