344e56eafd1120154b1f94bd91fb43407c805433
Daniil Belogur <katokinawa@icloud.com>
2022-10-30 11:41:10 +0300

    feat: avatar pen pointer

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -25,6 +25,15 @@ export const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+
+export const apiOption = {
+  url: 'https://mesto.nomoreparties.co/v1/cohort-52',
+    headers: {
+      authorization: 'e5ef6c8f-bf2d-4d8f-9be9-29b7fb227f3e',
+      'Content-Type': 'application/json'
+    },
+}
+
 export const nameInput = document.querySelector('.name-input');
 export const jobInput = document.querySelector('.job-input');
 export const editButton = document.querySelector('.profile__edit-button');
